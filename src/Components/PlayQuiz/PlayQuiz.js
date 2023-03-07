36b9c9d752aa97c32225d55030e7ff4c7288d793
SHITANSHU KUMAR MITHUN <shitanshukumar27@.com>
2023-03-07 22:08:04 +0530

    first commit

---
+++ src/Components/PlayQuiz/PlayQuiz.js
@@ -0,0 +1,124 @@
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getName, playQuiz } from "../Redux/Actions";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
+// Selecting the displayed and reated quizzes to be played //
+
+const QuizTitle = ({ title, id }) => {
+  const selectedQuiz = useRef();
+
+  const dispatch = useDispatch();
+
+  const handelSelected = () => {
+    const selected = selectedQuiz.current.checked;
+
+    //  if there is no quiz selected then do nothng //
+
+    if (!selected) {
+      return;
+    }
+
+    // dispatching and choosing the quiz to be played //
+
+    dispatch(playQuiz(id));
+  };
+
+  return (
+    <div className="created-quiz-container d-flex">
+      <input
+        type="radio"
+        name=""
+        id=""
+        ref={selectedQuiz}
+        onClick={handelSelected}
+      />
+      <p>{title}</p>
+    </div>
+  );
+};
+
+// This is all the logic of Playing quiz page //
+
+export const PlayQuiz = () => {
+  const name = useRef();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const quiz = useSelector((state) => state.reducer.quiz);
+
+  // this handler will handle and route us to play the quiz which has been selected //
+
+  const getNameHandler = () => {
+    // if there id no name entered then do nothing except an alert //
+
+    if (name.current.value === "") {
+      alert("Please enter a name!");
+      return;
+    }
+
+    // if there exist a quiz choose the selected one and let us play that quiz with the name entered and route to the play page //
+
+    if (quiz.length > 0) {
+      dispatch(getName(name.current.value));
+      navigate("/quiz");
+    }
+  };
+
+  //  if there is no quiz then this empty message will be displayed //
+
+  const emptyMsg = (
+    <p style={{ color: "red" }}>
+      There are Currently No Quiz! Please make some new quizzes!
+    </p>
+  );
+
+  return (
+    <motion.div
+      className="play-quiz-container"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.4 }}
+    >
+      <div className="play-quiz-main">
+        <div className="play-quiz-heading">
+          <h1>Title of the Quiz</h1>
+        </div>
+
+        <div className="quiz-description">
+       <p> Read the instructions carefully 
+       Consider the time you have.
+Read through the whole exam first.
+Read the questions carefully.
+Read the questions before text or graphic elements.
+Consider the answer choices carefully.
+Do final checks. </p>
+
+        </div>
+
+        <div className="input-name">
+          <div className="quiz-name">
+            <label>Enter Your Name</label>
+            <input type="text" ref={name} className="name-input-text" />
+          </div>
+          <div className="created-quiz">
+            {quiz.length === 0
+              ? emptyMsg
+              : quiz
+                  .filter((el) => el.isActive === true)
+                  .map((el) => (
+                    <QuizTitle title={el.title} key={el.id} id={el.id} />
+                  ))}
+          </div>
+        </div>
+
+        <div className="submti-name-btn">
+          <button style={{ margin: 0 }} onClick={getNameHandler}>
+            Start Quiz
+          </button>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
